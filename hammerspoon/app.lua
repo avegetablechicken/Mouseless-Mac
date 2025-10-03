796d4c18aed35c1f371932ff5305270849d9b17b
Yiwen Xu <xywthu16@163.com>
2025-10-04 04:38:31 +0800

    update

--- hammerspoon/app.lua
+++ hammerspoon/app.lua
@@ -7071,13 +7071,11 @@ local function wrapCondition(obj, config, mode)
         func(o)
       end
       return true
-    elseif result == CF.leftMenubarItemSelected then
-      selectMenuItemOrKeyStroke(app, mods, key)
-      return true
     elseif result == CF.rightMenubarItemSelected then
       safeGlobalKeyStroke(mods, key)
       return true
-    elseif result == CF.nonEmptyTextFieldFocused then
+    elseif result == CF.leftMenubarItemSelected
+        or result == CF.nonEmptyTextFieldFocused then
       hs.eventtap.keyStroke(mods, key, nil, app)
       return true
     elseif result == CF.uIElementNotFocused then
