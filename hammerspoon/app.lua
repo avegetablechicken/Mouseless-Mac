ecc6cef8d0a49e924cb0ba7b4a4024ce361325cb
Yiwen Xu <xywthu16@163.com>
2025-11-19 16:50:13 +0800

    support confirming deleting in more windows of `WPS Office`

--- hammerspoon/app.lua
+++ hammerspoon/app.lua
@@ -9033,7 +9033,7 @@ local specialConfirmFuncs = {
       if not btnName then return end
       local buttons = getc(winUI, AX.Button)
       for _, button in ipairs(buttons) do
-        if button.AXTitle == btnName then
+        if button.AXTitle:match('^'..btnName) then
           return button
         end
       end
