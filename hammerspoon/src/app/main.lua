30f2282e4988eb24d0e73aa9726f8bde1619c710
Yiwen Xu <xywthu16@163.com>
2026-03-21 00:24:27 +0800

    only export "menuBarMenuSelectedObservers" to app main namescope

--- hammerspoon/src/app/main.lua
+++ hammerspoon/src/app/main.lua
@@ -1,6 +1,22 @@
+local mainNamespace = {}
+
 local function loadMainPart(part)
   local path = hs.configdir .. "/src/app/main/" .. part .. ".lua"
-  local chunk, err = loadfile(path, "t", _ENV)
+  local partEnv = setmetatable({
+    exportMainSymbol = function(name, value)
+      mainNamespace[name] = value
+    end,
+  }, {
+    __index = function(_, key)
+      local value = mainNamespace[key]
+      if value ~= nil then
+        return value
+      end
+      return _ENV[key]
+    end,
+    __newindex = _ENV,
+  })
+  local chunk, err = loadfile(path, "t", partEnv)
   if not chunk then
     error(err)
   end
